8115f11c1cf407e0570dcbaf25e078599bd9330e
Xenonar <xenon_ark_88@hotmail.com>
2022-04-12 01:31:27 +0200

    add api authorized
    secure api

--- pages/api/create-stripe-customer.ts
+++ pages/api/create-stripe-customer.ts
@@ -2,6 +2,11 @@ import initStrip from 'stripe'
 import { supabase } from '../../utils/supabase';
 
 const handler = async (req, res) =>{
+    //checking that the req have passing API secret key or not
+    if(req.query.API_ROUTE_SECRET !== process.env.API_ROUTE_SECRET)
+    {
+        return res.status(401).send('You are not authorized to call this API!')
+    }
     const stripe = initStrip(process.env.STRIPE_SECRET);
 
     const customer = await stripe.customers.create({
